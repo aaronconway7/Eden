5a04d900f3110b997862da2ce332b09b7c36dd87
aaronconway7 <aaronconway796@gmail.com>
2016-11-29 12:33:37 +0000

    More tooltips

--- assets/js/function.js
+++ assets/js/function.js
@@ -13,6 +13,24 @@ function animatedScroll() {
     });
 }
 
+var wScroll = 0;
+var bannerTop = $('#banner .banner').offset().top;
+
+function fixedBanner(){
+    console.log(wScroll);
+    if(wScroll > bannerTop){
+        $('#banner .banner').addClass('is-fixed');
+    } else {
+        $('#banner .banner').removeClass('is-fixed');
+    }
+}
+
 $(document).ready(function() {
     animatedScroll();
+});
+
+$(window).scroll(function() {
+    wScroll = $(window).scrollTop();
+    animatedScroll();
+    fixedBanner();
 });

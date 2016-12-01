71a28a9637ba7996e69f2c0407a1f4bbb2af1eea
aaronconway7 <aaronconway796@gmail.com>
2016-12-01 12:57:53 +0000

    Modal finished?

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,12 +1,21 @@
 function openModal(){
     $('.open-modal').click(function(){
         $('.modal').addClass('is-open');
+        $('body').addClass('no-scroll');
     });
 }
 
 function closeModal(){
     $('.modal .close').click(function(){
         $('.modal').removeClass('is-open');
+        $('body').removeClass('no-scroll');
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+            $('.modal').removeClass('is-open');
+            $('body').removeClass('no-scroll');
+        }
     });
 }
 

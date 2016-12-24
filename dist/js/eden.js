2ac72d105c8054d56b7e24d21406adf7ed3f6e27
aaronconway7 <aaronconway796@gmail.com>
2016-12-24 15:01:07 +0000

    compiled

--- dist/js/eden.js
+++ dist/js/eden.js
@@ -1,35 +1,31 @@
-// function openModal(){
-//     $('.open-modal').click(function(){
-//         $('.modal').addClass('is-open');
-//         $('body').addClass('no-scroll');
-//     });
-// }
-//
-// function closeModal(){
-//     $('.modal .close').click(function(){
-//         $('.modal').removeClass('is-open');
-//         $('body').removeClass('no-scroll');
-//     });
-//
-//     $(document).keyup(function(e) {
-//         if (e.keyCode == 27) { // escape key maps to keycode `27`
-//             $('.modal').removeClass('is-open');
-//             $('body').removeClass('no-scroll');
-//         }
-//     });
-// }
-//
-// $(document).ready(function(){
-//     openModal();
-//     closeModal();
-// });
-
 (function($) {
 
-    $.fn.helloWorld = function() {
+    $.fn.modal = function(options) {
 
-        this.each( function() {
-            $(this).text("Hello, World!");
+        var modal = this;
+
+        // Establish our default settings
+        var settings = $.extend({
+            open    : '.open-modal',
+            close   : '.close-modal'
+        }, options);
+
+        return $(settings.open).click(function(){
+            modal.addClass('is-open');
+            $('body').addClass('no-scroll');
+
+            modal.find(settings.close).click(function(){
+                modal.removeClass('is-open');
+                $('body').removeClass('no-scroll');
+            });
+
+            $(document).keyup(function(e) {
+                if (e.keyCode == 27) { // escape key maps to keycode `27`
+                    modal.removeClass('is-open');
+                    $('body').removeClass('no-scroll');
+                }
+            });
+
         });
 
     }

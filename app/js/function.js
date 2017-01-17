2e987cf55b481d8bb706ef62405f0bbc68837286
aaronconway7 <aaronconway796@gmail.com>
2017-01-17 10:40:27 +0000

    lightbox js plugin

--- app/js/function.js
+++ app/js/function.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    lightbox();
+    $('.lightbox').lightbox();
     // $('.animation').animate();
     // $('.nav').nav({
     //     pageShift: true,
@@ -19,69 +19,76 @@ $(window).scroll(function(){
     // $('.hero-1').parallax();
 });
 
-function lightbox(){
-    $('.lightbox').each(function(){
-        $(this).append(
-            "<div class='modal dark-bg'>"+
-            "   <div class='content'>"+
-            "       <div class='active-img'>"+
-            "           <span class='slide-left slider-control'>❮</span>"+
-            "           <span class='slide-right slider-control'>❯</span>"+
-            "       </div>"+
-            "       <div class='close light inside'>"+
-            "           <span>✕</span>"+
-            "       </div>"+
-            "   </div>"+
-            "</div>"
-        )
-    });
+(function($){
+    $.fn.lightbox = function(){
 
-    $('.lightbox > img').click(function(){
-        if($(this).hasClass('is-active')){
+        var lightbox = this;
 
-        }else{
-            $(this).addClass('is-active').clone(true).appendTo('.lightbox .modal .active-img');
-            $(this).next('.caption').clone(true).appendTo('.lightbox .modal .active-img');
-            $(this).siblings('.modal').show();
-        }
-    });
+        return this.each(function(){
+            $(this).append(
+                "<div class='modal dark-bg'>"+
+                "   <div class='content'>"+
+                "       <div class='active-img'>"+
+                "           <span class='slide-left slider-control'>❮</span>"+
+                "           <span class='slide-right slider-control'>❯</span>"+
+                "       </div>"+
+                "       <div class='close light inside'>"+
+                "           <span>✕</span>"+
+                "       </div>"+
+                "   </div>"+
+                "</div>"
+            );
 
-    $('.lightbox .slider-control').click(function(){
-        if($(this).hasClass('slide-left')){
-            if($('.lightbox > img.is-active').is(':first-of-type')){
-                $('.lightbox > img.is-active').removeClass('is-active');
-                $('.lightbox > img:last-of-type').addClass('is-active');
-            }else{
-                $('.lightbox > img.is-active').removeClass('is-active').prevAll('img:first').addClass('is-active');
-            }
-        }else{
-            if($('.lightbox > img.is-active').is(':last-of-type')){
-                $('.lightbox > img.is-active').removeClass('is-active');
-                $('.lightbox > img:first-of-type').addClass('is-active');
-            }else{
-                $('.lightbox > img.is-active').removeClass('is-active').nextAll('img:first').addClass('is-active');
-            }
-        }
-        $('.lightbox .modal .active-img img').remove();
-        $('.lightbox .modal .active-img .caption').remove();
-        $('.lightbox > img.is-active').clone(true).appendTo('.lightbox .modal .active-img');
-        $('.lightbox > img.is-active').next('.caption').clone(true).appendTo('.lightbox .modal .active-img');
-    });
+            $(this).children('img').click(function(){
+                if(!$(this).hasClass('is-active')){
+                    $(this).addClass('is-active').clone(true).appendTo('.lightbox .modal .active-img');
+                    $(this).next('.caption').clone(true).appendTo('.lightbox .modal .active-img');
+                    $(this).siblings('.modal').addClass('is-open');
+                    $('body').addClass('no-scroll');
+                }
+            });
 
-    $(document).keydown(function(e){
-        if(e.keyCode == 37){ // left
-            $('.lightbox .slider-control.slide-left').trigger('click');
-        }else if(e.keyCode == 39){ // right
-            $('.lightbox .slider-control.slide-right').trigger('click');
-        }else if(e.keyCode == 27){ // esc
-            $('.lightbox .close').trigger('click');
-        }
-    });
+            $(this).find('.slider-control').click(function(){
+                if($(this).hasClass('slide-left')){
+                    if($(this).closest('.lightbox').children('img.is-active').is(':first-of-type')){
+                        $(this).closest('.lightbox').children('img.is-active').removeClass('is-active');
+                        $(this).closest('.lightbox').children('img:last-of-type').addClass('is-active');
+                    }else{
+                        $(this).closest('.lightbox').children('img.is-active').removeClass('is-active').prevAll('img:first').addClass('is-active');
+                    }
+                }else{
+                    if($(this).closest('.lightbox').children('img.is-active').is(':last-of-type')){
+                        $(this).closest('.lightbox').children('img.is-active').removeClass('is-active');
+                        $(this).closest('.lightbox').children('img:first-of-type').addClass('is-active');
+                    }else{
+                        $(this).closest('.lightbox').children('img.is-active').removeClass('is-active').nextAll('img:first').addClass('is-active');
+                    }
+                }
+                $(this).siblings('img').remove();
+                $(this).siblings('.caption').remove();
+                $(this).closest('.lightbox').children('img.is-active').clone(true).appendTo('.lightbox .modal .active-img');
+                $(this).closest('.lightbox').children('img.is-active').next('.caption').clone(true).appendTo('.lightbox .modal .active-img');
+            });
 
-    $('.lightbox .close').click(function(){
-        $('.modal').hide();
-        $('.lightbox .is-active').removeClass('is-active');
-        $('.lightbox .modal .active-img img').remove();
-        $('.lightbox .modal .active-img .caption').remove();
-    });
-}
+            $(this).find('.close').click(function(){
+                $(this).closest('.modal').removeClass('is-open');
+                $('body').removeClass('no-scroll');
+                $(this).closest('.lightbox').find('.is-active').removeClass('is-active');
+                $(this).siblings('.active-img').find('img').remove();
+                $(this).siblings('.active-img').find('.caption').remove();
+            });
+
+            $(document).keydown(function(e){
+                if(e.keyCode == 37){ // left
+                    lightbox.find('.slider-control.slide-left').trigger('click');
+                }else if(e.keyCode == 39){ // right
+                    lightbox.find('.slider-control.slide-right').trigger('click');
+                }else if(e.keyCode == 27){ // esc
+                    lightbox.find('.close').trigger('click');
+                }
+            });
+
+        });
+
+    }
+}(jQuery));
